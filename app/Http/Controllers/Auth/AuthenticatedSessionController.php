e8ae50f65a1340c554ac533b64790478a52c80f1
Tegar Ferdigantara <115655820+tegarferdigantara@users.noreply.github.com>
2024-07-26 21:09:14 +0700

    sign in ssr

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->route('admin.dashboard');
+        return redirect()->route('admin.dashboard')->with('success', 'Login Berhasil');
     }
 
     /**
